8657d9962033ab60c53d92aa4cfff0ed861b9de8
S A <sanden@sandenaverett.com>
2018-09-24 19:50:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  newArray = array.slice(1);
+  return newArray;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
